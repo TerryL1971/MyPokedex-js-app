e968cb39b3eb3c0065b152dac32546298399153d
TerryL1971 <tcl71771@yahoo.com>
2021-05-13 11:11:18 +0200

    Update scripts.js
    Added Pokemon array and labeled all variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,24 @@
-alert('Hello world');
-// Variables
-let favoriteFood = 'Watermelon';
-document.write(favoriteFood);
+
+/* // Pokemon 1
+let pokemon1Name = 'Bulbasaur';
+let pokemon1Type = ["Grass" , "Poison"];
+let pokemon1Height = 0.7
+let pokemon1Weight = 6.9
+
+// Pokemon 2
+let pokemon2Name = 'Charizard';
+let pokemon2Type = ['Fire' , 'Flying'];
+let pokemon2Height = 1.7
+let pokemon2Weight = 90.5
+
+// Pokemon 3
+let pokemon3Name = 'Mewtwo';
+let pokemon3Type = 'Psychic';
+let pokemon3Height = 2.0
+let pokemon3Weight = 122 */
+
+// This array contains Pokémon data to display in the application.
+let pokemonList = [{ pokemon1Name: 'Bulbasaur', pokemon1Type: ['Grass', 'Poison'], pokemon1Height: 0.7, pokemon1Weight: 6.9 },
+  { pokemon2Name: 'Charizard', pokemon2Type: ['Fire', 'Flying'], pokemon2Height: 1.7, pokemon2Weight: 90.5 },
+  { pokemon3Name: 'Mewtwo', pokemon3Type: ['Psychic'], pokemon3Height: 2.0, pokemon3Weight: 122 }];
+console.log(pokemonList);
