c912b0bb0da69fac30592121a4b6d5834d93a38c
TerryL1971 <tcl71771@yahoo.com>
2021-05-14 08:15:19 +0200

    Update scripts.js
    Updated syntax using string literals in the document.write lines.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
 // This array contains Pokémon data to display in the application.
 let pokemonList = [
     {
-         pokemonName: "Bulbasaur",
-         pokemonType: ["Grass", "Poison"],
-         pokemonHeight: 0.7,
-         pokemonWeight: 6.9
+         name: "Bulbasaur",
+         type: ["Grass", "Poison"],
+         height: 0.7,
+         weight: 6.9
      },
      {
-      pokemonName: "Charizard",
-      pokemonType: ["Fire', 'Flying"],
-      pokemonHeight: 1.7,
-      pokemonWeight: 90.5
+      name: "Charizard",
+      type: ["Fire', 'Flying"],
+      height: 1.7,
+      weight: 90.5
     },
     {
-      pokemonName: "Mewtwo",
-      pokemonType: ["Psychic"],
-      pokemonHeight: 2.0,
-      pokemonWeight: 122
+      name: "Mewtwo",
+      type: ["Psychic"],
+      height: 2.0,
+      weight: 122
     }
   ];
  console.log(pokemonList);
 
 for (let list=0; list < pokemonList.length; list++){
-  if (pokemonList[list].pokemonHeight > 0.1 && pokemonList[list].pokemonHeight < 1.7){
-    document.write(pokemonList[list].pokemonName + ": Height ", pokemonList[list].pokemonHeight + " <p></p>");
-}else if (pokemonList[list].pokemonHeight >= 1.7 && pokemonList[list].pokemonHeight < 2.0){
-    document.write(pokemonList[list].pokemonName + ": Height ", pokemonList[list].pokemonHeight + " <p></p>");
+  if (pokemonList[list].height > 0.1 && pokemonList[list].height < 1.7){
+    document.write(`${pokemonList[list].name}: Height ${pokemonList[list].height}` + " <p></p>");
+}else if (pokemonList[list].height >= 1.7 && pokemonList[list].height < 2.0){
+    document.write(`${pokemonList[list].name}: Height ${pokemonList[list].height}` + " <p></p>");
   }else {
-    document.write(pokemonList[list].pokemonName + ": Height  ", pokemonList[list].pokemonHeight + "  --Wow, that is a big Pokemon!");
+    document.write(`${pokemonList[list].name}: Height ${pokemonList[list].height}` + "  --Wow, that is a big Pokemon!");
   }
 }
